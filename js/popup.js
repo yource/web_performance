9813081bca93b05d17c0d21501e8aa1314a633c9
yangxiang <yangxiang@menusifu.cn>
2019-06-21 17:13:30 +0800

    v1.0

--- js/popup.js
+++ js/popup.js
@@ -13,7 +13,19 @@ $(function() {
     $(".item").click(function() {
         $(this).addClass("active").siblings().removeClass("active");
         $(".chart").hide();
-        $("#chart" + $(this).attr("id").substr(4, 1)).show();
+        var currentId = "#chart" + $(this).attr("id").substr(4, 1);
+        $(currentId).show();
+        if (currentId == "#chart1") {
+            $("#moreTime").show();
+        } else {
+            $("#moreTime").hide();
+        }
+    });
+
+    $("#tip").hover(function() {
+        $("#tipContent").fadeIn(200);
+    }, function() {
+        $("#tipContent").fadeOut(100);
     })
 
     sentToCurrentConent();
@@ -27,34 +39,38 @@ $(function() {
 function initChart() {
     chart1 = echarts.init(document.getElementById('chart1'));
     option1 = {
+        title: {
+            text: "TIME COUNTING",
+            x: "center",
+            y: 40
+        },
         color: ['#3398DB'],
         tooltip: {
             trigger: 'axis',
             axisPointer: {
                 type: 'shadow'
-            }
+            },
+            formatter: "{b} : {c} ms"
         },
         grid: {
             left: '3%',
             right: '4%',
             bottom: '3%',
+            top: 80,
             containLabel: true
         },
-        xAxis: [{
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisTick: {
-                alignWithLabel: true
-            }
-        }],
-        yAxis: [{
+        xAxis: {
             type: 'value'
-        }],
+        },
+        yAxis: {
+            type: 'category',
+            data: ['Other', 'Image', 'CSS', 'JS', 'XHR/Fetch']
+        },
         series: [{
             name: '直接访问',
             type: 'bar',
             barWidth: '60%',
-            data: [10, 52, 200, 334, 390, 330, 220]
+            data: [0, 0, 0, 0, 0, 0]
         }]
     };
     chart1.setOption(option1);
@@ -62,30 +78,38 @@ function initChart() {
     chart2 = echarts.init(document.getElementById('chart2'));
     option2 = {
         title: {
-            text: '某站点用户访问来源',
-            subtext: '纯属虚构',
-            x: 'center'
+            text: "REQUEST COUNTING",
+            x: "center",
+            y: 40
         },
         tooltip: {
             trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+            formatter: "{b} Request: <br/> {c} items ({d}%)"
         },
         series: [{
-            name: '访问来源',
+            name: '',
             type: 'pie',
             radius: '55%',
             center: ['50%', '60%'],
+            label: {
+                normal: {
+                    formatter: '{b|{b}}：{c|{c}}',
+                    rich: {
+                        b: {
+                            fontSize: 14
+                        },
+                        c: {
+                            fontSize: 15
+                        }
+                    }
+                }
+            },
             data: [
-                { value: 335, name: '直接访问' },
-                { value: 310, name: '邮件营销' },
-                { value: 234, name: '联盟广告' },
-                { value: 135, name: '视频广告' },
-                { value: 1548, name: '搜索引擎' }
+                { value: 0, name: 'XHR' },
+                { value: 0, name: 'JS' },
+                { value: 0, name: 'CSS' },
+                { value: 0, name: 'Img' },
+                { value: 0, name: 'Others' }
             ],
             itemStyle: {
                 emphasis: {
@@ -100,14 +124,27 @@ function initChart() {
 
     chart3 = echarts.init(document.getElementById('chart3'));
     option3 = {
-        tooltip: {
-            formatter: "{a} <br/>{b} : {c}%"
+        title: {
+            text: "MEMORY FOOTPRINT",
+            x: "center",
+            y: 40
         },
         series: [{
             name: 'Memory',
             type: 'gauge',
-            detail: { formatter: '{value} Mb' },
-            data: [{ value: 50, name: 'Used Memory' }]
+            min: 0,
+            max: 100,
+            center: ['50%', '60%'],
+            axisLine: { // 坐标轴线
+                lineStyle: { // 属性lineStyle控制线条样式
+                    width: 15
+                }
+            },
+            detail: {
+                formatter: '{value} Mb',
+                fontSize: 16
+            },
+            data: [{ value: 50, name: '' }]
         }]
     };
     chart3.setOption(option3);
@@ -130,17 +167,92 @@ function setPerformance(performance) {
     var requestNum = performance.entries.length;
     $("#allRequestNum").html(requestNum);
 
-    var usedMemory = performance.memory.usedJSHeapSize / (1024 * 1024);
-    $("#usedMemory").html(usedMemory.toFixed(2));
+    var usedMemory = (performance.memory.usedJSHeapSize / (1024 * 1024)).toFixed(2);
+    $("#usedMemory").html(usedMemory);
 
     var loadTime = (performance.timing.loadEventEnd - performance.timing.navigationStart) / 1000;
-    $("#loadTime").html(loadTime.toFixed(2));
+    $("#loadTime").html(loadTime < 0 ? "..." : loadTime.toFixed(2));
 
-    option3.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+    var tcpTime = (performance.timing.domainLookupEnd - performance.timing.domainLookupStart) + (performance.timing.connectEnd - performance.timing.connectStart);
+    $("#tcp").html(tcpTime < 0 ? "..." : tcpTime + "μs");
+
+    var domTime = performance.timing.domComplete - performance.timing.domInteractive;
+    $("#dom").html(domTime < 0 ? "..." : domTime + "μs");
+
+    var whiteTime = performance.timing.responseStart - performance.timing.navigationStart;
+    $("#white").html(whiteTime < 0 ? "..." : whiteTime + "μs");
+
+    var request_js = request_css = request_img = request_other = request_resource = request_ajax = 0;
+    var requestTime = {
+        js: 0,
+        css: 0,
+        xhr: 0,
+        img: 0,
+        other: 0,
+        all: 0,
+    };
+    for (var i = 0; i < requestNum; i++) {
+        var time0 = performance.entries[i].responseEnd - performance.entries[i].requestStart;
+        if (!time0 || time0 < 0) {
+            time0 = 0;
+        }
+        requestTime.all += time0;
+        if (performance.entries[i].entryType == "resource") {
+            request_resource++;
+            if (performance.entries[i].initiatorType == "script") {
+                request_js++;
+                requestTime.js += time0;
+            } else if (performance.entries[i].initiatorType == "link") {
+                request_css++;
+                requestTime.css += time0;
+            } else if (performance.entries[i].initiatorType == "img" || isImg(performance.entries[i].name)) {
+                request_img++;
+                requestTime.img += time0;
+            } else if (performance.entries[i].initiatorType == "xmlhttprequest" || performance.entries[i].initiatorType == "fetch") {
+                request_ajax++;
+                requestTime.xhr += time0;
+            }
+        } else {
+            request_other++;
+            requestTime.other += time0;
+        }
+    }
+
+    option1.series[0].data = [
+        requestTime.other.toFixed(2),
+        requestTime.img.toFixed(2),
+        requestTime.css.toFixed(2),
+        requestTime.js.toFixed(2),
+        requestTime.xhr.toFixed(2)
+    ];
+    chart1.setOption(option1, true);
+
+    option2.series[0].data = [
+        { value: request_ajax, name: 'XHR/Fetch' },
+        { value: request_js, name: 'JS' },
+        { value: request_css, name: 'CSS' },
+        { value: request_img, name: 'Image' },
+        { value: request_other, name: 'Others' }
+    ];
+    chart2.setOption(option2, true);
+
+
+    option3.series[0].max = (performance.memory.totalJSHeapSize / (1024 * 1024)).toFixed(1);
+    option3.series[0].data[0].value = usedMemory;
     chart3.setOption(option3, true);
 
-    if (score == 0) {
-        score = 99;
+    if (loadTime > 0 && requestNum > 0 && usedMemory > 0) {
+        score = (100 - loadTime - usedMemory).toFixed(1);
         $("#score").html(score);
+    } else {
+        $("#score").html("...");
     }
+}
+
+function isImg(url) {
+    url = url.toLowerCase();
+    if (url.indexOf(".png") > -1 || url.indexOf(".jpg") > -1 || url.indexOf(".jpeg") > -1 || url.indexOf(".gif") > -1 || url.indexOf(".bmp") > -1 || url.indexOf(".tif") > -1 || url.indexOf("svg") > -1) {
+        return true;
+    }
+    return false;
 }
